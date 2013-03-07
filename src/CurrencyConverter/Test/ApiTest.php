0b6e0bba87024decff609255e63de89d06cb3472
Bora Tunca <bora.tunca@yahoo.com>
2013-03-07 17:00:08 +0100

    getrate added

--- src/CurrencyConverter/Test/ApiTest.php
+++ src/CurrencyConverter/Test/ApiTest.php
@@ -28,6 +28,15 @@ class ApiTest extends \PHPUnit_Framework_TestCase
     /**
      *
      */
+    public function testGetRate()
+    {
+        $this->assertEquals(1.1, $this->api->getRate('JPY'));
+        $this->assertEquals(1.2, $this->api->getRate('BGN'));
+    }
+
+    /**
+     *
+     */
     public function testRefreshRates()
     {
         $this->browser->expects($this->once())

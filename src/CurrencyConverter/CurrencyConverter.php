30c94f0befe682124feab83ccc58f7cf7c91a9da
grandbora <bora.tunca@yahoo.com>
2013-03-09 00:12:31 +0100

    conver by string array added

--- src/CurrencyConverter/CurrencyConverter.php
+++ src/CurrencyConverter/CurrencyConverter.php
@@ -21,14 +21,47 @@ class CurrencyConverter
     }
 
     /**
+     * Converts to given amount from given currency to US currency
+     * Returns the value in US currency
      *
      * @param string $currency
      * @param int $amount
      * @return double
      */
-    public function convertToUSCurrency($currency, $amount)
+    public function convert($currency, $amount)
     {
         $rate = $this->api->getRate($currency);
         return $rate * $amount;
+    }
+
+    /**
+     * Converts the amount given in string to US currency
+     * String is expected in "{currency} {amount}" format
+     * Returns the value in same string format
+     *
+     * @param string $amountStr
+     * @return string
+     */
+    public function convertByString($amountStr)
+    {
+        $amountData = explode(' ', $amountStr); 
+        $convertedValue = $this->convert($amountData[0], (double)$amountData[1]);
+
+        return "USD $convertedValue";
+    }
+
+    /**
+     * Converts the amounts given in string array to US currency
+     * String is expected in "{currency} {amount}" format
+     * Returns the value in same string format in array
+     *
+     * @param array $amountStr
+     * @return array
+     */
+    public function convertByArray(array $amountList)
+    {
+        return array_map(function($amountString) {
+            return $this->convertByString($amountString);
+        }, $amountList);
     }
 }
--- src/CurrencyConverter/Test/Unit/CurrencyConverterTest.php
+++ src/CurrencyConverter/Test/Unit/CurrencyConverterTest.php
@@ -39,10 +39,52 @@ class CurrencyConverterTest extends \PHPUnit_Framework_TestCase
             ->with($this->equalTo('TEST_CURRENCY2'))
             ->will($this->returnValue(1.673));
 
-        $actual = $this->currencyConverter->convertToUSCurrency('TEST_CURRENCY1', 550);
+        $actual = $this->currencyConverter->convert('TEST_CURRENCY1', 550);
         $this->assertSame(7.5185, $actual);
 
-        $actual = $this->currencyConverter->convertToUSCurrency('TEST_CURRENCY2', 400);
+        $actual = $this->currencyConverter->convert('TEST_CURRENCY2', 400);
         $this->assertSame(669.2, $actual);
+    }
+
+    /**
+     *
+     */
+    public function testConvertByString()
+    {
+        $this->api->expects($this->at(0))
+            ->method('getRate')
+            ->with($this->equalTo('TEST_CURRENCY1'))
+            ->will($this->returnValue(0.01367));
+        $this->api->expects($this->at(1))
+            ->method('getRate')
+            ->with($this->equalTo('TEST_CURRENCY2'))
+            ->will($this->returnValue(1.673));
+
+        $actual = $this->currencyConverter->convertByString('TEST_CURRENCY1 550');
+        $this->assertSame('USD 7.5185', $actual);
+
+        $actual = $this->currencyConverter->convertByString('TEST_CURRENCY2 400');
+        $this->assertSame('USD 669.2', $actual);
+    }
+
+    /**
+     *
+     */
+    public function testConvertByArray()
+    {
+        $this->api->expects($this->at(0))
+            ->method('getRate')
+            ->with($this->equalTo('TEST_CURRENCY1'))
+            ->will($this->returnValue(0.01367));
+        $this->api->expects($this->at(1))
+            ->method('getRate')
+            ->with($this->equalTo('TEST_CURRENCY2'))
+            ->will($this->returnValue(1.673));
+
+        $actual = $this->currencyConverter->convertByArray(
+            array('TEST_CURRENCY1 550','TEST_CURRENCY2 400'));
+        $expected = array('USD 7.5185','USD 669.2');
+
+        $this->assertSame($expected, $actual);
     }
 }

008b2da71827fa8dec8a19c932f2717a6f80fbaa
grandbora <bora.tunca@yahoo.com>
2013-03-09 14:48:26 +0100

    usd added

--- web/js/view/currencylist.js
+++ web/js/view/currencylist.js
@@ -4,7 +4,7 @@ define(function(){
     attributes:{
       'data-role' : 'fieldcontain'
     }
-    
+
    ,render: function() {
 
       var label = $('<label>').attr('for', 'select-currency')
@@ -12,8 +12,10 @@ define(function(){
       var select = $('<select>').attr('name', 'select-currency')
         .attr('id', 'select-currency')
 
+      var option = $('<option>').val(1).text('USD')
+      select.append(option)
       _.each(this.options.data, function(value, key, list){
-        var option = $('<option>').val(value).text(key)
+        option = $('<option>').val(value).text(key)
         select.append(option)
       })
 
